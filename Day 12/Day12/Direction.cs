4ef61aa5c268727aa95d8862bfc3d2d461f91563
joko-dev <jokodev@outlook.de>
2021-01-16 16:46:33 +0100

    Day 12: Rain Risk - First Part

---
+++ Day 12/Day12/Direction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day12
+{
+    class Direction
+    {
+
+        public const char NORTH = 'N';
+        public const char SOUTH = 'S';
+        public const char EAST = 'E';
+        public const char WEST = 'W';
+        public const char LEFT = 'L';
+        public const char RIGHT = 'R';
+        public const char FORWARDS = 'F';
+        
+        public char Action { get; }
+        public int Value { get; }
+
+        public Direction(string line)
+        {
+            Action = line[0];
+            Value = Int32.Parse(line.Substring(1));
+        }
+
+        public Direction(char action, int value)
+        {
+            Action = action;
+            Value = value;
+        }
+    }
+}
---
+++ Day 12/Day12/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position currentPosition = new Position(0, 0, Direction.EAST);
+
+            Console.WriteLine("Dirextion list:");
+            string filepath = Console.ReadLine();
+            List<Direction> directions = GetDirectionPath(filepath);
+
+            foreach (Direction direction in directions)
+            {
+                currentPosition.Move(direction);
+            }
+
+            Console.WriteLine("Manhatten distance: {0}", currentPosition.getManhattanDistance());
+        }
+
+        private static List<Direction> GetDirectionPath(string filepath)
+        {
+            List<Direction> directions = new List<Direction>();
+
+            string fileInput = File.ReadAllText(filepath);
+
+            fileInput = fileInput.Replace("\r\n", "\n");
+
+            string[] lines = fileInput.Split("\n");
+            foreach (string line in lines)
+            {
+                directions.Add(new Direction(line));
+            }
+
+            return directions;
+        }
+    }
+
+}
